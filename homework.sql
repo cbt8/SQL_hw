65b0240062e156f43c47e6a2bd2a70e664ffa38f
Diogenes <cbt8@rice.edu>
2019-02-06 19:40:31 -0600

    first commit

---
+++ homework.sql
@@ -0,0 +1,67 @@
+USE sakila;
+
+#1a 
+SELECT first_name, last_name
+FROM actor;
+
+#1b
+SELECT CONCAT(first_name, " ", last_name) FROM actor;
+
+#2a
+SELECT actor_id, first_name, last_name FROM actor WHERE first_name = "Joe";
+
+#2b
+SELECT actor_id, first_name, last_name FROM actor WHERE last_name LIKE "%GEN%";
+
+#2c
+SELECT actor_id, first_name, last_name FROM actor WHERE last_name LIKE "%LI%"
+ORDER BY last_name, first_name;
+
+#2d
+SELECT country_id, country FROM country WHERE country IN ("Afghanistan", "Bangladesh", "China")
+
+#3a
+ALTER TABLE actor ADD description BLOB;
+
+#3b
+ALTER TABLE actor DROP description;
+
+#4a
+SELECT COUNT(last_name), last_name FROM actor GROUP BY last_name; 
+
+#4b
+SELECT COUNT(last_name), last_name FROM actor GROUP BY last_name HAVING COUNT(last_name) >1; 
+
+#4c
+UPDATE actor SET first_name = "HARPO" WHERE first_name="GROUCHO" AND last_name = "WILLIAMS";
+
+#4d
+UPDATE actor SET first_name = "GROUCHO" WHERE first_name="HARPO" AND last_name = "WILLIAMS";
+
+#5a
+SHOW CREATE TABLE address;
+
+#6a
+
+SELECT staff.first_name, staff.last_name, address.address FROM staff
+JOIN address ON staff.address_id=address.address_id;
+
+#6b
+SELECT first_name, last_name, sum(amount), payment.staff_id
+FROM staff
+INNER JOIN payment
+    ON staff.staff_id = payment.staff_id
+WHERE payment.payment_date LIKE "%-08-%"
+GROUP BY first_name    
+
+#6c
+
+INNER JOIN payment
+    ON staff.staff_id = payment.staff_id
+
+
+
+#Having a strange problem here, where staff_id shows up both as 1 and 2 when I view each table separately, but when I try to join, the join only displays
+#staff_id 1
+SELECT* from payment
+SELECT* from staff
