7f74409c27def532354ae53fc45fb8556dfd1754
Diogenes <cbt8@rice.edu>
2019-02-11 19:04:10 -0600

    third commit

--- homework.sql
+++ homework.sql
@@ -81,3 +81,11 @@ FROM payment
 INNER JOIN customer
 ON  payment.customer_id = customer.customer_id 
 GROUP BY last_name
+
+#7a
+
+SELECT title
+FROM film
+WHERE title LIKE "K%" OR title LIKE "Q%" AND language_id = 
+( SELECT language_id FROM language WHERE name = "English")
+
